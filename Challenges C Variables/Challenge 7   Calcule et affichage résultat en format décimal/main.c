2f8b0ef65e09976161fd11c55c62f637b76c52ff
fouadrouane1 <fouadfirdaous66@gmail.com>
2021-11-12 10:03:16 +0100

    transfer les challenge 7 de langage c variables

---
+++ Challenges C Variables/Challenge 7   Calcule et affichage résultat en format décimal/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a,b;
+	printf("entrer la valeur de a");
+	scanf("%d",&a);
+	printf("entrer la valeur de b");
+	scanf("%d",&b);
+	printf("a+b=%d \n",a+b);
+	printf("a-b=%d \n",a-b);
+	printf("a*b=%d \n",a*b);
+	printf("a/b=%d \n",a/b);
+	printf("a%b=%d \n",a%b);
+	
+	return 0;
+}
