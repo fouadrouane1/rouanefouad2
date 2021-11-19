4acaf894c3d29db315d6208a18c167583a8ed9d4
fouadrouane1 <fouadfirdaous66@gmail.com>
2021-11-19 12:23:40 +0100

    modifie le challenge 2

--- Challenges boucles/Challenge 2 pyramide d'étoile/main.c
+++ Challenges boucles/Challenge 2 pyramide d'étoile/main.c
@@ -4,6 +4,19 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int i , l , e , t ;
+	printf ("entrer un nombre :");
+	scanf ("%d",&l);
+	for (i=1 ; i<=l ; i++)
+		{
+		for (e=1 ; e<=l-i ; e++){
+			printf (" ");
+		}
+		for (t=1 ; t<=(i*2)-1 ;t++ ){
+			printf("*");
+		}
+		printf ("\n");
+	} 
 	
 	return 0;
 }
